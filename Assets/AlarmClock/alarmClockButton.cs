d4c4c157e1c7b117c9a441541bf98497f1dcdb41
JSleekXD <julianlaserage@hotmail.com>
2016-06-17 20:11:34 +0100

    Adding Running Late Larry to Github
    Adding all the source files to Github

---
+++ Assets/AlarmClock/alarmClockButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class alarmClockButton : MonoBehaviour {
+
+	void OnTriggerStay2D(Collider2D other) {
+
+		//Debug.Log ("collided");
+		if (Input.GetKeyDown (KeyCode.Space)) {
+		//	Debug.Log ("win");
+		}
+	}
+}
